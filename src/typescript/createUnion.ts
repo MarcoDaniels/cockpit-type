030fcb9ffa2250fabb0eabac70fe3c44c7ac2e1f
Marco Martins <marcodanielmartins@gmail.com>
2020-12-29 21:43:00 +0100

    Handle Repeater type

--- src/typescript/createUnion.ts
+++ src/typescript/createUnion.ts
@@ -1,3 +1,7 @@
 export const createUnion = (data: string[]) => `'${data.join('\' | \'')}'`
 
 export const createUnionMultiple = (data: string[]) => `('${data.join('\' | \'')}')[]`
+
+export const createUnionType = (data: string[]) => `${data.join(' | ')}`
+
+export const createUnionTypeMultiple = (data: string[]) => `${data.join(' | ')}[]`
