a1bac4dbf7cee393975f7017d5ab32a54916eab0
Marco Martins <marcodanielmartins@gmail.com>
2021-01-04 21:06:21 +0100

    Setup wrapper for cli

--- src/index.ts
+++ src/index.ts
@@ -1,74 +1,19 @@
-import { NodePlopAPI } from 'plop'
-import { cockpitClient } from './cockpit/cockpitClient'
-import { schemaTemplate } from './typescript/schemaTemplate'
-import { format, resolveConfig } from 'prettier'
+#!/usr/bin/env node
+import path from 'path'
+import { Plop, run } from 'plop'
 
-export type Answers = {
-    filterBy: 'collection' | 'group' | 'none'
-    filter: string
-    prefix?: string
-}
+const args = process.argv.slice(2)
+const argv = require('minimist')(args)
 
-export default (plop: NodePlopAPI) =>
-    plop.setGenerator(`generate`, {
-        description: 'Generates types for Cockpit',
-        prompts: [
-            {
-                type: 'input',
-                name: 'path',
-                message: 'Destination File:',
-                validate: (path: string) => {
-                    if (/(.*\.(?:d.ts|ts))/i.test(path)) return true
-
-                    return 'Please provide a valid TypeScript file path'
-                },
-            },
-            {
-                type: 'list',
-                name: 'filterBy',
-                message: 'Filter by:',
-                choices: [
-                    { name: 'None', value: 'none' },
-                    { name: 'Collection', value: 'collection' },
-                    { name: 'Group', value: 'group' },
-                ],
-            },
-            {
-                type: 'input',
-                name: 'filter',
-                message: 'Filter Name:',
-                when: (answers: any) => answers.filterBy !== 'none',
-            },
-            {
-                type: 'input',
-                name: 'prefix',
-                message: 'Prefix:',
-            },
-        ],
-        actions: [
-            {
-                type: 'add',
-                path: '{{path}}',
-                templateFile: 'template/typescript.hbs',
-                force: true,
-                transform: async (template: string, answers: Answers) => {
-                    switch (answers.filterBy) {
-                        case 'collection':
-                            const collectionResponse = await cockpitClient.collectionSchema(answers.filter)
-                            if (collectionResponse.type === 'success')
-                                template += schemaTemplate(answers.prefix)(collectionResponse.data)
-
-                            return format(template, { parser: 'babel-ts', ...resolveConfig.sync(`.prettierrc`) })
-                        default:
-                            const response = await cockpitClient.collections(answers.filter)
-                            if (response.type === 'success')
-                                response.data
-                                    .map(schemaTemplate(answers.prefix))
-                                    .map((schemaTemplate) => (template += schemaTemplate))
-
-                            return format(template, { parser: 'babel-ts', ...resolveConfig.sync(`.prettierrc`) })
-                    }
-                },
-            },
-        ],
-    })
+Plop.launch(
+    {
+        cwd: argv.cwd,
+        configPath: path.join(__dirname, 'plopfile.js'),
+        require: argv.require,
+        completion: argv.completion,
+    },
+    (env) => {
+        const options = { ...env, dest: process.cwd() }
+        return run(options, undefined, true)
+    },
+)
---
+++ src/plopfile.ts
@@ -0,0 +1,74 @@
+import { NodePlopAPI } from 'plop'
+import { cockpitClient } from './cockpit/cockpitClient'
+import { schemaTemplate } from './typescript/schemaTemplate'
+import { format, resolveConfig } from 'prettier'
+
+export type Answers = {
+    filterBy: 'collection' | 'group' | 'none'
+    filter: string
+    prefix?: string
+}
+
+export default (plop: NodePlopAPI) =>
+    plop.setGenerator(`generate`, {
+        description: 'Generates types for Cockpit',
+        prompts: [
+            {
+                type: 'input',
+                name: 'path',
+                message: 'Destination File:',
+                validate: (path: string) => {
+                    if (/(.*\.(?:d.ts|ts))/i.test(path)) return true
+
+                    return 'Please provide a valid TypeScript file path'
+                },
+            },
+            {
+                type: 'list',
+                name: 'filterBy',
+                message: 'Filter by:',
+                choices: [
+                    { name: 'None', value: 'none' },
+                    { name: 'Collection', value: 'collection' },
+                    { name: 'Group', value: 'group' },
+                ],
+            },
+            {
+                type: 'input',
+                name: 'filter',
+                message: 'Filter Name:',
+                when: (answers: any) => answers.filterBy !== 'none',
+            },
+            {
+                type: 'input',
+                name: 'prefix',
+                message: 'Prefix:',
+            },
+        ],
+        actions: [
+            {
+                type: 'add',
+                path: '{{path}}',
+                templateFile: 'template/typescript.hbs',
+                force: true,
+                transform: async (template: string, answers: Answers) => {
+                    switch (answers.filterBy) {
+                        case 'collection':
+                            const collectionResponse = await cockpitClient.collectionSchema(answers.filter)
+                            if (collectionResponse.type === 'success')
+                                template += schemaTemplate(answers.prefix)(collectionResponse.data)
+
+                            return format(template, { parser: 'babel-ts', ...resolveConfig.sync(`.prettierrc`) })
+                        default:
+                            const response = await cockpitClient.collections(answers.filter)
+                            if (response.type === 'success')
+                                response.data
+                                    .map(schemaTemplate(answers.prefix))
+                                    .map((schemaTemplate) => (template += schemaTemplate))
+
+                            return format(template, { parser: 'babel-ts', ...resolveConfig.sync(`.prettierrc`) })
+                    }
+                },
+            },
+        ],
+    })
