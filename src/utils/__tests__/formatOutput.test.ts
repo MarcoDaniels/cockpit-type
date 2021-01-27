fe4801907ad7bcd9e03319e3b67719407b060365
Marco Martins <marcodanielmartins@gmail.com>
2021-01-27 19:03:49 +0100

    Test setup with ava

---
+++ src/utils/__tests__/formatOutput.test.ts
@@ -0,0 +1,16 @@
+import test from 'ava'
+import { formatOutput } from '../formatOutput'
+
+test('formatOutput with TypeScript type', async (t) => {
+    const template = `type User = { name: string; }`
+    const output = await formatOutput(template, 'typescript')
+
+    t.is(output, 'type User = { name: string }\n')
+})
+
+test('formatOutput with Scala case class', async (t) => {
+    const template = `case class User(name: String)`
+    const output = await formatOutput(template, 'scala')
+
+    t.is(output, template)
+})
