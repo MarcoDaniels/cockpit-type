9cf4632667bb4690830cca616ce5ab5fe4f1245f
Marco Martins <marcodanielmartins@gmail.com>
2021-02-20 19:55:06 +0100

    Setup cockpit-http-client

--- src/plop/plopTransform.ts
+++ src/plop/plopTransform.ts
@@ -1,10 +1,12 @@
 import { plopFilterBy } from './plopFilterBy'
-import { cockpitClient } from '../cockpit/cockpitClient'
+import { cockpitClient } from 'cockpit-http-client'
 import { CockpitSchemaTemplate, cockpitSchemaTemplate } from '../cockpit/cockpitSchemaTemplate'
 import { maker as LanguageMaker } from '../maker/maker'
 import { PlopPrompt } from './plopPrompt'
 import { formatOutput } from '../utils/formatOutput'
 import { plopValidateLangFile } from './plopValidateLangFile'
+import { config } from '../utils/config'
+import { FieldSchema } from '../cockpit/cockpitTypes'
 
 export const plopTransform = async (template: string, answers: PlopPrompt): Promise<string> => {
     const validation = plopValidateLangFile(answers)
@@ -12,34 +14,38 @@ export const plopTransform = async (template: string, answers: PlopPrompt): Prom
         return validation
     }
 
+    const client = cockpitClient({ apiURL: config.cockpitAPIURL, apiToken: config.cockpitAPIToken })
+
     const schema: CockpitSchemaTemplate = { maker: LanguageMaker(answers.language), prefix: answers.prefix }
     const filters = plopFilterBy(answers.filter)
 
     if (filters) {
         switch (filters.filterBy) {
             case 'collection': {
-                const collection = await cockpitClient.collectionSchema(filters.filterName)
-                if (collection.type === 'success') {
-                    template += cockpitSchemaTemplate(schema)(collection.data)
+                const collection = await client.collections.schema(filters.filterName)
+                if (collection.success) {
+                    template += cockpitSchemaTemplate(schema)(collection.data as FieldSchema)
                 }
 
                 return formatOutput(template, answers.language)
             }
             case 'singleton': {
-                const singleton = await cockpitClient.singletonSchema(filters.filterName)
-                if (singleton.type === 'success') {
-                    template += cockpitSchemaTemplate(schema)(singleton.data)
+                const singleton = await client.singletons.schema(filters.filterName)
+                if (singleton.success) {
+                    template += cockpitSchemaTemplate(schema)(singleton.data as FieldSchema)
                 }
 
                 return formatOutput(template, answers.language)
             }
             case 'group': {
-                const collection = await cockpitClient.collections(filters.filterName)
-                const singleton = await cockpitClient.singletons(filters.filterName)
-                if (collection.type === 'success' && singleton.type === 'success') {
-                    collection.data
-                        .concat(singleton.data)
-                        .map(cockpitSchemaTemplate(schema))
+                const collection = await client.collections.schemas()
+                const singleton = await client.singletons.schemas()
+
+                if (collection.success && singleton.success) {
+                    Object.values(collection.data)
+                        .concat(Object.values(singleton.data))
+                        .filter((d) => d.group && d.group === filters.filterName)
+                        .map((data) => cockpitSchemaTemplate(schema)(data as FieldSchema))
                         .map((schemaTemplate) => (template += schemaTemplate))
                 }
 
@@ -48,12 +54,13 @@ export const plopTransform = async (template: string, answers: PlopPrompt): Prom
         }
     }
 
-    const collection = await cockpitClient.collections()
-    const singleton = await cockpitClient.singletons()
-    if (collection.type === 'success' && singleton.type === 'success') {
-        collection.data
-            .concat(singleton.data)
-            .map(cockpitSchemaTemplate(schema))
+    const collection = await client.collections.schemas()
+    const singleton = await client.singletons.schemas()
+
+    if (collection.success && singleton.success) {
+        Object.values(collection.data)
+            .concat(Object.values(singleton.data))
+            .map((data) => cockpitSchemaTemplate(schema)(data as FieldSchema))
             .map((schemaTemplate) => (template += schemaTemplate))
     }
 
